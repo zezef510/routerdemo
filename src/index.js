c00e12443bf99b9e66d83e934d32247a72f64f49
zezef510 <zezefff510@gmail.com>
2023-06-21 08:54:39 +0700

    2106

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import http from 'http';
+import fs from 'fs';
+import qs from 'qs';
+import url from 'url';
+
+const server = http.createServer((req,res)=>{
+
+})
+
+server.listen("8080", ()=>{
+    console.log("Da ket noi")
+})
