b4f01b4d9d6b844184c8e0d62606ed661aeb23b7
zezef510 <zezefff510@gmail.com>
2023-06-23 11:33:37 +0700

    chua chinh sua

--- src/index.js
+++ src/index.js
@@ -3,20 +3,33 @@ import fs from 'fs';
 import qs from 'qs';
 import url from 'url';
 import router from "./router/router.js";
-import {ProductService} from "./service/productService.js";
 
+let mimeTypes = {
+    'jpg': 'images/jpg',
+    'png': 'images/png',
+    'js': 'text/javascript',
+    'css': 'text/css',
+    'svg': 'image/svg+xml',
+    'ttf': 'font/ttf',
+    'woff': 'font/woff',
+    'woff2': 'font/woff2',
+    'eot': 'application/vnd.ms-fontobject'
+}
 const server = http.createServer((req, res) => {
-    let urlObject = url.parse(req.url, true)
-    let handle = router[urlObject.pathname];
-    if (handle === undefined) {
-        handle = router['/err']
+    let urlPath = url.parse(req.url).pathname;
+    const filesDefences = urlPath.match(/\.js|\.css|\.png|\.svg|\.jpg|\.ttf|\.woff|\.woff2|\.eot/);
+    if (filesDefences) {
+        const extension = mimeTypes[filesDefences[0].toString().split('.')[1]];
+        res.writeHead(200, {'Content-Type': extension});
+        fs.createReadStream(process.cwd() + req.url).pipe(res)
+    } else {
+        let handle = router[urlPath];
+        if (handle === undefined) {
+            handle = router['/err'];
+        }
+        handle(req, res);
     }
-    handle(req, res);
-
-
-})
-
-
-server.listen("8080", () => {
-    console.log("Da ket noi")
+});
+server.listen('8080', () => {
+    console.log('Đã kết nối')
 })
